e191fda066169513042ade1c85d3ccdd0cc44474
nagiinagendra <153055569+nagiinagendra@users.noreply.github.com>
2023-12-14 15:36:33 +0530

    Add files via upload

---
+++ conditional stat.py
@@ -0,0 +1,32 @@
+#a=6
+#r=a%2
+#if r==0:
+ #   print("it is even")
+#if r==1:
+ #   print ("it is odd")
+
+#b=345
+#x=b%2
+#if x==0:
+ #   print("it is even")
+#else:
+ #   print("it is odd")
+    
+#c=7787
+#y=c%2
+#if y==0:
+ #   print ("it is even")
+#if c>5:
+ #   print("it is greater")
+#else:
+ #   print("it is not greater")
+#if y!=0:
+ #   print("it is odd")    
+    
+age=18
+if age>18:
+    print("ready to vote")
+elif age<18:
+    print("not ready to vote")
+else:
+    print("waiting to vote")        
---
+++ forloop.py
@@ -0,0 +1,7 @@
+
+num=[10,20,30,44,55,66]
+for i in num:
+    if i%5==0:
+        print("it is divisible")
+else:
+    print("not divisible")
